7204d387672b2a0c53309585a26d8978568610ee
abdoNakhle <nakhleabdo2@gmail.com>
2026-05-04 12:14:22 +0400

    create splash view body

---
+++ lib/features/Splash/presentation/view_models/views/splash_view.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_app/features/Splash/presentation/view_models/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashViewBody();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/features/Splash/presentation/view_models/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
